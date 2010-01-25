51836b04178cd11dc386332ef85600157c42fbc8
Mikeal Rogers <mikeal.rogers@gmail.com>
2010-01-24 16:16:55 -0800

    Cleanup. Added licenes block and description.

--- ngiref.js
+++ ngiref.js
@@ -1,3 +1,28 @@
+/*
+This is a reference implementation of NGI (Node Gateway Interface).
+This implementation in github.com/mikeal/ngi should track with the specification for NGI in the same repository.
+License is as follows, uses same license on node.js
+
+Copyright 2010 Mikeal Rogers. All rights reserved.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to
+deal in the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+ 
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+ 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+IN THE SOFTWARE.
+*/
+
 var http = require("http"),
     sys = require("sys"),
     url = require("url");
@@ -107,7 +132,15 @@ var testApplication = function (connection, request, response) {
   response.end();
 };
 
-var s = new Server(testApplication, 8080);
-s.start();
+var runTest = function () {
+  var s = new Server(testApplication, 8080);
+  s.start();
+}
 
+exports.Server = Server;
+exports.Connection = Connection;
+exports.Response = Response;
+exports.Request = Request;
+
+// runTest();
 
